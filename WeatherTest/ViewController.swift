65208ac3b65e5b3a5d79400d53175fac9550df1b
Harnashevich <garnash1313@gmail.com>
2022-11-20 13:11:25 +0300

    Initial Commit

---
+++ WeatherTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Harnashevich on 20.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+
+
+}
+
